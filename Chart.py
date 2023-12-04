fa78857d42d0c7f87e2b28222eeddb134158ab65
Elliott Jackson <elliott.w.jackson@gmail.com>
2023-12-04 13:51:49 -0700

    add_datapoint()

--- Chart.py
+++ Chart.py
@@ -1,5 +1,6 @@
 import copy
 
+
 #
 # set up the values for the screen locations of the outer grid.  The
 # values to be set up correspond to the following diagram, where the
@@ -29,15 +30,23 @@ import copy
 #    **************************************************************
 #
 
+
 class Series:
 
-    def __init__(self, plot_char: str):
-        # what character is used to plot this series
-        self.plot_char = plot_char
+    # type declaration
+    point_list: list[tuple]
 
+    def __init__(self):
         # create empty list of points
-        self.pointList = list()
+        self.point_list = list()
 
+    # def __init__(self, series_id: str):
+    #     # what character is used to identify this series, and also to plot this series
+    #     # best to make this a single character, rather than a full string
+    #     self.series_id = series_id
+    #
+    #     # create empty list of points
+    #     self.point_list = list()
 
 
 class Chart:
@@ -46,24 +55,25 @@ class Chart:
 
     Chart has many Series, each of which has many tuples of (x, y) raw data points
     """
+    series_dict: dict[str, Series]
 
-    def __init__(self, num_series: int = 1):
+    # type declaration
 
-        # create the list of num_series series
-        self.seriesList = list()
-        for i in range(num_series):
-            # each series is a list of points
-            s = Series('1')
-            self.seriesList.append(s)
+    def __init__(self):
+
+        # create the dictionary of series, with key = series_id, val = Series object
+        # best to make the keys a single character, rather than a full string, since they are also
+        # used to plot that particular series
+        self.series_dict = {}
 
         # the plotting range will be 115 columns wide, and 74 rows tall
-        self.plotRange = (115, 74)
+        self.plot_range = (115, 74)
 
         # the plotting surface will be offset 11 columns from the left, and 3 rows from the top
-        self.plotOffset = (11, 3)
+        self.plot_offset = (11, 3)
 
-        num_cols = self.plotRange[0]
-        num_rows = self.plotRange[1]
+        num_cols = self.plot_range[0]
+        num_rows = self.plot_range[1]
 
         # the array of lines where we will 'draw' the plotted data
         self.lines = []
@@ -78,15 +88,49 @@ class Chart:
         for i in range(num_rows):
             self.lines.append(copy.copy(blank_line))
 
+    def add_datapoint(self, x: int, y: int, series_id: str = '1'):
+
+        # which series will this data point be added to?
+
+        # if there is not already a series with the passed series_id, create a new one
+        if series_id not in self.series_dict:
+            self.series_dict[series_id] = Series()
+
+        # get the desired Series
+        point = (x, y)
+        s = self.series_dict[series_id]
+        s.point_list.append(point)
+
     def draw(self):
 
+        # find_bounds()
+        # draw_axes()
+        # draw each series
+
+        # print them to the screen
         for ll in self.lines:
             print(ll)
+
+
+    def find_bounds(self):
+
+        pass
+
 
 
 def main():
 
     c = Chart()
+
+    c.add_datapoint(1, 2)
+    c.add_datapoint(3, 6)
+    c.add_datapoint(5, 4)
+    c.add_datapoint(-1, 3)
+
+    c.add_datapoint(11, 21, '2')
+    c.add_datapoint(31, 61, '2')
+    c.add_datapoint(51, 41, '2')
+
     c.draw()
 
 
